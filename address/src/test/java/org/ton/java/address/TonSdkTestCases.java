e6fc384274d8a02a150b131bdd0c8451b02e54a6
neodiX <neodix42@ton.org>
2024-06-19 09:50:26 +0200

    add address tests according to ton-sdk-use-cases rules

---
+++ address/src/test/java/org/ton/java/address/TonSdkTestCases.java
@@ -0,0 +1,33 @@
+package org.ton.java.address;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import java.io.Serializable;
+import java.util.Map;
+
+@Builder
+@Getter
+@Setter
+@ToString
+public class TonSdkTestCases implements Serializable {
+    private Map<String, TestCase> testCases;
+
+    @Setter
+    @Getter
+    public static class TestCase {
+
+        private String description;
+        private Map<String, Object> input;
+        private Map<String, Object> expectedOutput;
+
+        @Override
+        public String toString() {
+            return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+        }
+    }
+}
