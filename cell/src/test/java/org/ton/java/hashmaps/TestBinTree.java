289ea7b2f1606b1c56f6ffbcf03f8cf71c87f6a5
h6x0r <iteldom5@gmail.com>
2024-08-20 10:40:04 +0500

    improvements BinTree tests

--- cell/src/test/java/org/ton/java/hashmaps/TestBinTree.java
+++ cell/src/test/java/org/ton/java/hashmaps/TestBinTree.java
@@ -21,6 +21,173 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Slf4j
 @RunWith(JUnit4.class)
 public class TestBinTree {
+    private void printLog(Object o) {
+        log.info("ShardDescr: {}", o);
+    }
+
+    @Test
+    public void testBinTreeDeque() {
+        Deque<ShardDescr> cells = generateShardDescrDeque(5);
+        Cell c = BinTree.fromDeque(cells).toCell();
+
+        List<ShardDescr> bt = BinTree.deserialize(CellSlice.beginParse(c)).toList();
+
+        for (ShardDescr cc : bt) {
+            printLog(cc);
+        }
+
+        assertThat(bt.size()).isEqualTo(5);
+    }
+
+    @Test
+    public void testBinTree() {
+        BinTree node = generateBinTree(10);
+        Deque<ShardDescr> cells = generateShardDescrDeque(10);
+
+        Cell cTree = BinTree.fromDeque(cells).toCell();
+        Cell cRoot = node.toCell();
+
+        BinTree tree = BinTree.deserialize(CellSlice.beginParse(cTree));
+        BinTree root = BinTree.deserialize(CellSlice.beginParse(cRoot));
+        assertThat(tree).isNotNull();
+        assertThat(root).isNotNull();
+
+        List<ShardDescr> deserializedTree = tree.toList();
+        List<ShardDescr> deserializedRoot = root.toList();
+        deserializedRoot.sort((a, b) -> Math.toIntExact(a.getSeqNo() - b.getSeqNo()));
+
+        for (int i = 0; i < 10; i++) {
+            assertThat(deserializedRoot.get(i).getSeqNo()).isEqualTo(deserializedTree.get(i).getSeqNo());
+            printLog(deserializedRoot.get(i));
+        }
+
+        assertThat(deserializedTree.size()).isEqualTo(10);
+        assertThat(deserializedRoot.size()).isEqualTo(10);
+    }
+
+    @Test
+    public void testBinTreeEmpty() {
+        Deque<ShardDescr> cells = new ArrayDeque<>();
+        BinTree tree = BinTree.fromDeque(cells);
+        BinTree root = new BinTree();
+
+        assertThat(tree).isNull();
+        assertThat(root).isNotNull();
+    }
+
+
+    @Test
+    public void testBinTreeOne() {
+        Deque<ShardDescr> cells = generateShardDescrDeque(1);
+        BinTree root = generateBinTree(1);
+
+        Cell cTree = BinTree.fromDeque(cells).toCell();
+        Cell cRoot = root.toCell();
+
+        List<ShardDescr> deserializedTree = BinTree.deserialize(CellSlice.beginParse(cTree)).toList();
+        List<ShardDescr> deserializedRoot = BinTree.deserialize(CellSlice.beginParse(cRoot)).toList();
+
+        for (ShardDescr cc : deserializedTree) {
+            printLog(cc);
+        }
+
+        assertThat(deserializedTree.size()).isEqualTo(1);
+        assertThat(deserializedRoot.size()).isEqualTo(1);
+    }
+
+    @Test
+    public void testBinTreeTwo() {
+        Deque<ShardDescr> cells = generateShardDescrDeque(2);
+        BinTree root = generateBinTree(2);
+
+        Cell cTree = BinTree.fromDeque(cells).toCell();
+        Cell cRoot = root.toCell();
+
+        List<ShardDescr> deserializedTree = BinTree.deserialize(CellSlice.beginParse(cTree)).toList();
+        List<ShardDescr> deserializedRoot = BinTree.deserialize(CellSlice.beginParse(cRoot)).toList();
+
+        for (ShardDescr cc : deserializedTree) {
+            printLog(cc);
+        }
+
+        assertThat(deserializedTree.size()).isEqualTo(2);
+        assertThat(deserializedRoot.size()).isEqualTo(2);
+    }
+
+    @Test
+    public void testBinTreeThree() {
+        Deque<ShardDescr> cells = generateShardDescrDeque(3);
+        BinTree root = generateBinTree(3);
+
+        Cell cTree = BinTree.fromDeque(cells).toCell();
+        Cell cRoot = root.toCell();
+
+        printLog(cTree);
+        printLog(cRoot);
+
+        List<ShardDescr> deserializedTree = BinTree.deserialize(CellSlice.beginParse(cTree)).toList();
+        List<ShardDescr> deserializedRoot = BinTree.deserialize(CellSlice.beginParse(cRoot)).toList();
+
+        for (ShardDescr cc : deserializedTree) {
+            printLog(cc);
+        }
+
+        assertThat(deserializedTree.size()).isEqualTo(3);
+        assertThat(deserializedRoot.size()).isEqualTo(3);
+    }
+
+    @Test
+    public void testBinTreeLarge() {
+        BinTree root = generateBinTree(100);
+
+        Cell cRoot = root.toCell();
+
+        printLog(cRoot);
+
+        List<ShardDescr> deserializedRoot = BinTree.deserialize(CellSlice.beginParse(cRoot)).toList();
+
+        for (ShardDescr cc : deserializedRoot) {
+            printLog(cc);
+        }
+
+        assertThat(deserializedRoot.size()).isEqualTo(100);
+    }
+
+
+    private Deque<ShardDescr> generateShardDescrDeque(int sz) {
+        Deque<ShardDescr> cells = new ArrayDeque<>();
+
+        for (int i = 0; i < sz; i++) {
+            cells.add(createShardDescr(i + 1));
+        }
+
+        return cells;
+    }
+
+    public BinTree generateBinTree(int size) {
+        ShardDescr[] shardDescrArr = new ShardDescr[size];
+
+        for (int i = 0; i < size; i++) {
+            shardDescrArr[i] = createShardDescr(i + 1);
+        }
+
+        return buildTree(shardDescrArr, 0, size - 1);
+    }
+
+    private BinTree buildTree(ShardDescr[] shardDescrs, int start, int end) {
+        if (start > end) {
+            return null;
+        }
+
+        int mid = (start + end) / 2;
+        ShardDescr value = shardDescrs[mid];
+
+        BinTree left = buildTree(shardDescrs, start, mid - 1);
+        BinTree right = buildTree(shardDescrs, mid + 1, end);
+
+        return new BinTree(value, left, right);
+    }
+
 
     private ShardDescr createShardDescr(int seqno) {
         return ShardDescr.builder()
@@ -53,174 +220,5 @@ public class TestBinTree {
                         .coins(BigInteger.valueOf(2))
                         .build())
                 .build();
-    }
-
-    private void printLog(Object o) {
-        log.info("ShardDescr: {}", o);
-    }
-
-    @Test
-    public void testBinTreeDeque() {
-        ShardDescr shardDescr1 = createShardDescr(1);
-        ShardDescr shardDescr2 = createShardDescr(2);
-        ShardDescr shardDescr3 = createShardDescr(3);
-        ShardDescr shardDescr4 = createShardDescr(4);
-        ShardDescr shardDescr5 = createShardDescr(5);
-
-        Deque<ShardDescr> cells = new ArrayDeque<>();
-        cells.add(shardDescr1);
-        cells.add(shardDescr2);
-        cells.add(shardDescr3);
-        cells.add(shardDescr4);
-        cells.add(shardDescr5);
-
-        Cell c = BinTree.fromDeque(cells).toCell();
-
-        List<ShardDescr> bt = BinTree.deserialize(CellSlice.beginParse(c)).toList();
-
-        for (ShardDescr cc : bt) {
-            printLog(cc);
-        }
-
-        assertThat(bt.size()).isEqualTo(5);
-    }
-
-    @Test
-    public void testBinTree() {
-        ShardDescr shardDescr1 = createShardDescr(1);
-        ShardDescr shardDescr2 = createShardDescr(2);
-        ShardDescr shardDescr3 = createShardDescr(3);
-        ShardDescr shardDescr4 = createShardDescr(4);
-        ShardDescr shardDescr5 = createShardDescr(5);
-        ShardDescr shardDescr6 = createShardDescr(6);
-        ShardDescr shardDescr7 = createShardDescr(7);
-        ShardDescr shardDescr8 = createShardDescr(8);
-        ShardDescr shardDescr9 = createShardDescr(9);
-        ShardDescr shardDescr10 = createShardDescr(10);
-
-        BinTree tree10 = new BinTree(shardDescr10);
-        BinTree tree9 = new BinTree(shardDescr9, tree10, null);
-        BinTree tree8 = new BinTree(shardDescr8);
-        BinTree tree7 = new BinTree(shardDescr7, tree8, tree9);
-        BinTree tree6 = new BinTree(shardDescr6);
-        BinTree tree5 = new BinTree(shardDescr5, tree6, tree7);
-        BinTree tree4 = new BinTree(shardDescr4, null, tree5);
-        BinTree tree3 = new BinTree(shardDescr3);
-        BinTree tree2 = new BinTree(shardDescr2, tree3, null);
-
-        BinTree root = new BinTree(shardDescr1, tree2, tree4);
-
-        Deque<ShardDescr> cells = new ArrayDeque<>();
-        cells.add(shardDescr1);
-        cells.add(shardDescr2);
-        cells.add(shardDescr3);
-        cells.add(shardDescr4);
-        cells.add(shardDescr5);
-        cells.add(shardDescr6);
-        cells.add(shardDescr7);
-        cells.add(shardDescr8);
-        cells.add(shardDescr9);
-        cells.add(shardDescr10);
-
-        Cell cRootFromDeque = BinTree.fromDeque(cells).toCell();
-        Cell cRoot = root.toCell();
-
-        BinTree tree = BinTree.deserialize(CellSlice.beginParse(cRootFromDeque));
-        BinTree node = BinTree.deserialize(CellSlice.beginParse(cRoot));
-        assertThat(tree).isNotNull();
-        assertThat(node).isNotNull();
-
-        List<ShardDescr> deserializedTree = tree.toList();
-        List<ShardDescr> deserializedNode = node.toList();
-        for (int i = 0; i < 10; i++) {
-            assertThat(deserializedNode.get(i).getSeqNo()).isEqualTo(deserializedTree.get(i).getSeqNo());
-            printLog(deserializedTree.get(i));
-        }
-
-        assertThat(deserializedTree.size()).isEqualTo(10);
-        assertThat(deserializedNode.size()).isEqualTo(10);
-    }
-
-    @Test
-    public void testBinTreeEmpty() {
-        Deque<ShardDescr> cells = new ArrayDeque<>();
-        BinTree tree = BinTree.fromDeque(cells);
-        BinTree root = new BinTree();
-
-        assertThat(tree).isNull();
-        assertThat(root).isNotNull();
-    }
-
-
-    @Test
-    public void testBinTreeOne() {
-        ShardDescr shardDescr1 = createShardDescr(1);
-
-        Deque<ShardDescr> cells = new ArrayDeque<>();
-        cells.add(shardDescr1);
-
-        BinTree root = new BinTree(shardDescr1);
-
-        Cell cTree = BinTree.fromDeque(cells).toCell();
-        Cell cRoot = root.toCell();
-
-        List<ShardDescr> deserializedTree = BinTree.deserialize(CellSlice.beginParse(cTree)).toList();
-        List<ShardDescr> deserializedRoot = BinTree.deserialize(CellSlice.beginParse(cRoot)).toList();
-
-        for (ShardDescr cc : deserializedTree) {
-            printLog(cc);
-        }
-
-        assertThat(deserializedTree.size()).isEqualTo(1);
-        assertThat(deserializedRoot.size()).isEqualTo(1);
-    }
-
-    @Test
-    public void testBinTreeTwo() {
-        ShardDescr shardDescr1 = createShardDescr(1);
-        ShardDescr shardDescr2 = createShardDescr(2);
-
-        Deque<ShardDescr> cells = new ArrayDeque<>();
-        cells.add(shardDescr1);
-        cells.add(shardDescr2);
-
-        BinTree root = new BinTree(shardDescr1, new BinTree(shardDescr1), null);
-
-        Cell cTree = BinTree.fromDeque(cells).toCell();
-        Cell cRoot = root.toCell();
-
-        List<ShardDescr> deserializedTree = BinTree.deserialize(CellSlice.beginParse(cTree)).toList();
-        List<ShardDescr> deserializedRoot = BinTree.deserialize(CellSlice.beginParse(cRoot)).toList();
-
-        for (ShardDescr cc : deserializedTree) {
-            printLog(cc);
-        }
-
-        assertThat(deserializedTree.size()).isEqualTo(2);
-        assertThat(deserializedRoot.size()).isEqualTo(2);
-    }
-
-    @Test
-    public void testBinTreeThree() {
-        ShardDescr shardDescr1 = createShardDescr(1);
-        ShardDescr shardDescr2 = createShardDescr(2);
-        ShardDescr shardDescr3 = createShardDescr(3);
-
-        Deque<ShardDescr> cells = new ArrayDeque<>();
-        cells.add(shardDescr1);
-        cells.add(shardDescr2);
-        cells.add(shardDescr3);
-
-        Cell c = BinTree.fromDeque(cells).toCell();
-
-        printLog(c);
-
-        List<ShardDescr> bt = BinTree.deserialize(CellSlice.beginParse(c)).toList();
-
-        for (ShardDescr cc : bt) {
-            printLog(cc);
-        }
-
-        assertThat(bt.size()).isEqualTo(3);
     }
 }
