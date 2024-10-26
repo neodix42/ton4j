4907064e9954ed4ebedd99db651bf9b8622e30f5
h6x0r <inteldom5@gmail.com>
2024-10-26 22:47:56 +0500

    wallet v5 usdt gasless transaction example (fixed wallet connection)

--- smartcontract/src/test/java/org/ton/java/smartcontract/integrationtests/GaslessTransactionTest.java
+++ smartcontract/src/test/java/org/ton/java/smartcontract/integrationtests/GaslessTransactionTest.java
@@ -31,6 +31,7 @@ import org.ton.java.tlb.types.Account;
 import org.ton.java.tlb.types.AccountStateActive;
 import org.ton.java.tlb.types.Message;
 import org.ton.java.tonlib.Tonlib;
+import org.ton.java.tonlib.types.ExtMessageInfo;
 import org.ton.java.utils.Utils;
 import org.ton.schema.gasless.SignRawMessage;
 import org.ton.schema.gasless.SignRawParams;
@@ -43,7 +44,7 @@ import org.ton.tonapi.sync.methods.GaslessMethod;
 @Slf4j
 public class GaslessTransactionTest {
 
-    private static final String API_KEY = "AGAGTULGTS2A7WIAAAAJWNBAJIN53OJIORIGI3DUTJVQTCMNOEZRUCSZNQ5MA3TL6WPDQ5I";
+    private static final String API_KEY = "AGAGTULGORVJQBAAAAAELZDTJD5KSFEUVFQOI6WCBOZB5SVYSTWUMAKTU6TOXBXZF5M6ZPQ";
 
     private static final Address recipient = Address.of("UQB8ANV_ynITQr1qHXADHDKYUAQ9VFcCRDZB7h4aPuPKuFtm");
     private static final Address USDT_MASTER = Address.of("EQCxE6mUtQJKFnGfaROTKOt1lZbDiiX1kCixRv7Nw2Id_sDs");
@@ -64,17 +65,17 @@ public class GaslessTransactionTest {
 
         Pair keyPairSrc = Mnemonic.toKeyPair(srcSeed);
 
-        log.info("public-key {}", bytesToHex(keyPairSrc.getPublicKey()));
-        log.info("secret-key {}", bytesToHex(keyPairSrc.getSecretKey()));
+        log.info("public-key {}", Utils.bytesToHex(keyPairSrc.getPublicKey()));
+        log.info("secret-key {}", Utils.bytesToHex(keyPairSrc.getSecretKey()));
 
-        TweetNaclFast.Signature.KeyPair keyPairSigSrc = TweetNaclFast.Signature.keyPair_fromSecretKey(keyPairSrc.getSecretKey());
+        TweetNaclFast.Signature.KeyPair keyPairSigSrc = TweetNaclFast.Signature.keyPair_fromSeed(keyPairSrc.getSecretKey());
 
+        long walletId = 2147483409L;
         // connect to a source wallet
         WalletV5 srcWallet = WalletV5.builder()
-            .wc(0)
             .tonlib(tonlib)
-            .walletId(2147483409L)
             .isSigAuthAllowed(true)
+            .walletId(walletId)
             .keyPair(keyPairSigSrc)
             .build();
 
@@ -109,34 +110,35 @@ public class GaslessTransactionTest {
         JettonWalletStableCoin srcJettonWallet = usdtMasterWallet.getJettonWallet(srcWalletAddress);
         log.info("my jettonWallet balance: {}", Utils.formatJettonValue(srcJettonWallet.getBalance(), 6, 2));
 
-        WalletV5Config walletV5Config = WalletV5Config.builder()
-            .walletId(2147483409L)
-            .seqno(srcWallet.getSeqno())
-//            .amount(Utils.toNano(0.2))
-            .queryId(HighloadQueryId.fromSeqno(1).getQueryId())
-            .body(srcWallet.createBulkTransfer(
+        Cell bodyCell = srcWallet.createBulkTransfer(
                 Collections.singletonList(
                     Destination.builder()
-//                        .address(recipient.toString())
+                        .address(recipient.toString())
                         .body(JettonWalletStableCoin.createTransferBody(
                             0,
                             BigInteger.valueOf(1_000_000),              // 1 USDT
                             recipient,                            // recipient
                             null,                                 // response address
                             BigInteger.ONE,                       // forward amount
-                            MsgUtils.createTextMessageBody("test gasless") // forward payload
-                        )).build()))
-                    .toCell()
-            ).build();
+                            MsgUtils.createTextMessageBody("test gasless") // comment
+                        ))
+                        .build()))
+            .toCell();
 
-        Message msg = srcWallet.prepareExternalMsg(walletV5Config);
+        WalletV5Config walletV5Config = WalletV5Config.builder()
+            .walletId(walletId)
+            .seqno(srcWallet.getSeqno())
+            .body(bodyCell)
+            .build();
 
-        Utils.sleep(90, "transferring 1 USDT jettons to wallet " + recipient);
+        ExtMessageInfo msg = srcWallet.send(walletV5Config);
+
+        Utils.sleep(3, "transferring 1 USDT jettons to wallet " + recipient);
 
         // Gasless flow
         GaslessMethod gaslessMethod = new GaslessMethod(tonApiClient);
 
-        String transferMessage = msg.toCell().toHex();
+        String transferMessage = msg.getHash();
         log.info("msg {}", transferMessage);
         String pubKey = bytesToHex(keyPairSigSrc.getPublicKey());
 
@@ -147,7 +149,7 @@ public class GaslessTransactionTest {
             USDT_MASTER.toString(),
             srcWalletAddress.toString(),
             pubKey,
-            Collections.singletonList(transferMessage)
+            Arrays.asList(transferMessage)
         );
 
         // signRawParams is the same structure as signRawParams in tonconnect.
