56b75157366040282bb7c6133988be25fb210bf9
neodix <neodix@ton.org>
2025-07-14 11:15:15 +0300

    add simple counter tolk v1.0 smc and test it against MyLocalTon Docker

--- tolk/src/test/java/org/ton/ton4j/tolk/TestTolkRunner.java
+++ tolk/src/test/java/org/ton/ton4j/tolk/TestTolkRunner.java
@@ -22,7 +22,36 @@ public class TestTolkRunner {
     String absolutePath = tolkFile.getAbsolutePath();
 
     TolkRunner tolkRunner =
-        TolkRunner.builder().tolkExecutablePath(Utils.getTolkGithubUrl()).build();
+        TolkRunner.builder().tolkExecutablePath(Utils.getArtifactGithubUrl("tolk","tolk-1.0.0")).build();
+
+    String result = tolkRunner.run(tolkFile.getParent(), absolutePath);
+    log.info("output: {}", result);
+  }
+
+  @Test
+  public void testTolkRunner1v() throws URISyntaxException {
+
+    URL resource = TestTolkRunner.class.getResource("/test-1.0.tolk");
+    File tolkFile = Paths.get(resource.toURI()).toFile();
+    String absolutePath = tolkFile.getAbsolutePath();
+
+    TolkRunner tolkRunner =
+            TolkRunner.builder()
+                    .tolkExecutablePath(Utils.getArtifactGithubUrl("tolk","tolk-1.0.0")).build();
+
+    String result = tolkRunner.run(tolkFile.getParent(), absolutePath);
+    log.info("output: {}", result);
+  }
+
+  @Test
+  public void testTolkRunnerCompiler() throws URISyntaxException {
+
+    URL resource = TestTolkRunner.class.getResource("/test-1.0.tolk");
+    File tolkFile = Paths.get(resource.toURI()).toFile();
+    String absolutePath = tolkFile.getAbsolutePath();
+
+    TolkRunner tolkRunner =
+            TolkRunner.builder().tolkExecutablePath(Utils.getArtifactGithubUrl("tolk","tolk-1.0.0")).build();
 
     String result = tolkRunner.run(tolkFile.getParent(), absolutePath);
     log.info("output: {}", result);
